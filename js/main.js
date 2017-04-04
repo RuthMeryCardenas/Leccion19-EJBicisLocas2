6e9c6222f6eecd20557785246f55df9ea41dc396
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-04 13:57:09 -0500

    Correción de validaciones

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
-var filters = [/[A-Z][a-z]+/g,//name
-               /[A-Z][a-z]+/g,//lastName
-               /([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})/,//email
-               /([\w-_.@]){6,30}/g,//password
-               /^[^0]///bicycle
-             ];
-var controlForm = document.getElementsByClassName('form-control');
-var tooltipSpan = document.getElementsByTagName('span');
-  controlForm[0].addEventListener('blur', function (){
-    if (filters[0].test(controlForm[0].value)) {
-      tooltipSpan[0].style.display = 'none';
+var messages = [];
+function validationMessages () {
+  messages[0] = 'Debe ingresar su nombre';
+  messages[1] = 'Debe ingresar su apellido';
+  messages[2] = 'Debe ingresar su e-mail';
+  messages[3] = 'Debe ingresar contraseña';
+  messages[4] = 'Debe seleccionar al menos un tipo de bici';
+  messages[5] = 'Su nombre debe empezar con mayúscula';
+  messages[6] = 'Su apellido debe empezar con mayúscula';
+  messages[7] = 'Verifique su e-mail';
+  messages[8] = 'La contraseña debe tener al menos 6 caracteres';
+  messages[9] = 'La contraseña debe ser diferente';
+  messages[10] = '';
+}
+function showHide (spanIndex, messageIndex, action){
+  var tooltipSpan = document.getElementsByTagName('span');
+  tooltipSpan[spanIndex].innerHTML = messages[messageIndex];
+  tooltipSpan[spanIndex].style.display = action;
+}
+function validateForm () {
+  validationMessages ();
+  var correctDataCounter = 0;
+  var formControl = document.getElementsByClassName('form-control');
+  var tooltipSpan = document.getElementsByTagName('span');
+  var validations = [/[A-Z][a-z]+/g,//name
+                 /[A-Z][a-z]+/g,//lastName
+                 /([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})/,//email
+                 /([\w-_.@]){6,30}/g,//password
+                 /^[^\s]///bicycle
+               ];
+  for (var i = 0; i < formControl.length - 1; i++) {
+    if (formControl[i].value == '') {
+      showHide (i, i, 'block');
     }else {
-      controlForm[0].focus();
+      if (validations[i].test(formControl[i].value)) {
+        if (i != 3) {
+          showHide (i, 10, 'none');
+          correctDataCounter ++
+        }else {
+          if (formControl[i].value != "password" && formControl[i].value != "123456" && formControl[i].value != "098754") {
+            showHide (i, 10, 'none');
+            correctDataCounter ++
+          }else {
+            showHide (i, i + 6, 'block');
+          }
+        }
+      }else {
+        showHide (i, i + 5, 'block');
+      }
     }
-  });
-  controlForm[1].addEventListener('blur', function (){
-    if (filters[1].test(controlForm[1].value)) {
-      tooltipSpan[1].style.display = 'none';
-    }else {
-      controlForm[1].focus();
-    }
-  });
-
-function validateForm(){
+  }
+  if (correctDataCounter == 5) {
+    console.log('Los datos han sido enviados correctamente');
+    document.getElementsByClassName('form-signup')[0].reset();
+  }
 }
